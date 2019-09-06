6de2443df7b73e911f0757c3ec405d93ea50e9da
Varathan <varathansuyambu95@gmail.com>
2019-09-06 13:39:13 +0530

    word add in to disable file options

---
+++ GWAddIn_2/GWAddIn_2/Connect.cs
@@ -0,0 +1,119 @@
+namespace GWAddIn_2
+{
+	using System;
+	using Extensibility;
+	using System.Runtime.InteropServices;
+    using Office = Microsoft.Office.Core;
+    using Word = Microsoft.Office.Interop.Word;
+    using System.IO;
+
+	#region Read me for Add-in installation and setup information.
+	// When run, the Add-in wizard prepared the registry for the Add-in.
+	// At a later time, if the Add-in becomes unavailable for reasons such as:
+	//   1) You moved this project to a computer other than which is was originally created on.
+	//   2) You chose 'Yes' when presented with a message asking if you wish to remove the Add-in.
+	//   3) Registry corruption.
+	// you will need to re-register the Add-in by building the GWAddIn_2Setup project, 
+	// right click the project in the Solution Explorer, then choose install.
+	#endregion
+	
+	/// <summary>
+	///   The object for implementing an Add-in.
+	/// </summary>
+	/// <seealso class='IDTExtensibility2' />
+	[GuidAttribute("F43DBF39-D2EC-4F0F-8903-5CE0CE034A12"), ProgId("GWAddIn_2.Connect")]
+    public class Connect : Object, Extensibility.IDTExtensibility2, Office.IRibbonExtensibility
+	{
+		/// <summary>
+		///		Implements the constructor for the Add-in object.
+		///		Place your initialization code within this method.
+		/// </summary>
+		public Connect()
+		{
+		}
+
+		/// <summary>
+		///      Implements the OnConnection method of the IDTExtensibility2 interface.
+		///      Receives notification that the Add-in is being loaded.
+		/// </summary>
+		/// <param term='application'>
+		///      Root object of the host application.
+		/// </param>
+		/// <param term='connectMode'>
+		///      Describes how the Add-in is being loaded.
+		/// </param>
+		/// <param term='addInInst'>
+		///      Object representing this Add-in.
+		/// </param>
+		/// <seealso class='IDTExtensibility2' />
+		public void OnConnection(object application, Extensibility.ext_ConnectMode connectMode, object addInInst, ref System.Array custom)
+		{
+            applicationObject = (Word.Application)application;
+			addInInstance = addInInst;
+		}
+
+		/// <summary>
+		///     Implements the OnDisconnection method of the IDTExtensibility2 interface.
+		///     Receives notification that the Add-in is being unloaded.
+		/// </summary>
+		/// <param term='disconnectMode'>
+		///      Describes how the Add-in is being unloaded.
+		/// </param>
+		/// <param term='custom'>
+		///      Array of parameters that are host application specific.
+		/// </param>
+		/// <seealso class='IDTExtensibility2' />
+		public void OnDisconnection(Extensibility.ext_DisconnectMode disconnectMode, ref System.Array custom)
+		{
+		}
+
+		/// <summary>
+		///      Implements the OnAddInsUpdate method of the IDTExtensibility2 interface.
+		///      Receives notification that the collection of Add-ins has changed.
+		/// </summary>
+		/// <param term='custom'>
+		///      Array of parameters that are host application specific.
+		/// </param>
+		/// <seealso class='IDTExtensibility2' />
+		public void OnAddInsUpdate(ref System.Array custom)
+		{
+		}
+
+		/// <summary>
+		///      Implements the OnStartupComplete method of the IDTExtensibility2 interface.
+		///      Receives notification that the host application has completed loading.
+		/// </summary>
+		/// <param term='custom'>
+		///      Array of parameters that are host application specific.
+		/// </param>
+		/// <seealso class='IDTExtensibility2' />
+		public void OnStartupComplete(ref System.Array custom)
+		{
+		}
+
+		/// <summary>
+		///      Implements the OnBeginShutdown method of the IDTExtensibility2 interface.
+		///      Receives notification that the host application is being unloaded.
+		/// </summary>
+		/// <param term='custom'>
+		///      Array of parameters that are host application specific.
+		/// </param>
+		/// <seealso class='IDTExtensibility2' />
+		public void OnBeginShutdown(ref System.Array custom)
+		{
+		}
+
+        private Word.Application applicationObject;
+		private object addInInstance;
+        string Office.IRibbonExtensibility.GetCustomUI(string RibbonID)
+        {
+            //return File.ReadAllText(@"C:\GSC\GWaddin.xml");
+            return GWAddIn_2.Properties.Resources.GWaddin;
+        }
+        private Office.IRibbonUI ribbon;
+        public void OnLoad(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+        }
+	}
+}
